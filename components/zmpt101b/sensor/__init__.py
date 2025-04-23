5f502a96dffc14c598f76b2ba63e40859efbc20b
Andrey <b_andy@tut.by>
2025-04-23 13:49:40 +0300

    Update __init__.py

--- components/zmpt101b/sensor/__init__.py
+++ components/zmpt101b/sensor/__init__.py
@@ -7,13 +7,26 @@ sensor_ns = cg.esphome_ns.namespace('zmpt101b_ns')
 
 Zmpt101bSensor = sensor_ns.class_('ZMPT101BSensor', cg.PollingComponent)
 
-CONFIG_SCHEMA = sensor.SENSOR_SCHEMA.extend({
+# Обновлённая схема конфигурации
+CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(Zmpt101bSensor),
-    cv.Required("adc_pin"): cv.int_range(min=0, max=99),
-    cv.Optional("sensitivity", default=500): cv.float_range(min=0, max=1000)
+    cv.Required("adc_pin"): cv.int_range(min=0, max=99),  # Пин АЦП
+    cv.Optional("sensitivity", default=500): cv.float_range(min=0, max=1000),  # Чувствительность
+    cv.Optional("rms_sensor"): sensor.SENSOR_SCHEMA,  # Датчик RMS-напряжения
+    cv.Optional("instant_sensor"): sensor.SENSOR_SCHEMA,  # Датчик мгновенного напряжения
 })
 
-def to_code(config):
+async def to_code(config):
+    # Создаём объект ZMPT101BSensor
     var = cg.new_Pvariable(config[CONF_ID], config["adc_pin"], config["sensitivity"])
-    yield cg.register_component(var, config)
-    yield sensor.register_sensor(var, config)
+    await cg.register_component(var, config)
+
+    # Регистрируем датчик RMS-напряжения
+    if "rms_sensor" in config:
+        sens_rms = await sensor.new_sensor(config["rms_sensor"])
+        cg.add(var.set_rms_sensor(sens_rms))
+
+    # Регистрируем датчик мгновенного напряжения
+    if "instant_sensor" in config:
+        sens_instant = await sensor.new_sensor(config["instant_sensor"])
+        cg.add(var.set_instant_sensor(sens_instant))
