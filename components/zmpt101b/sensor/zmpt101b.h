72d1f4efb2fa7a7fb27129fdcfec9afdeb5d87e4
Andrey <b_andy@tut.by>
2025-04-23 13:44:29 +0300

    Update zmpt101b.h

--- components/zmpt101b/sensor/zmpt101b.h
+++ components/zmpt101b/sensor/zmpt101b.h
@@ -7,14 +7,14 @@
 #define DEFAULT_SENSITIVITY 500.0f
 
 #if defined(AVR)
-	#define ADC_SCALE 1023.0f
-	#define VREF 5.0f
+    #define ADC_SCALE 1023.0f
+    #define VREF 5.0f
 #elif defined(ESP8266)
-	#define ADC_SCALE 1023.0
-	#define VREF 3.3
+    #define ADC_SCALE 1023.0
+    #define VREF 3.3
 #elif defined(ESP32)
-	#define ADC_SCALE 4095.0
-	#define VREF 3.3
+    #define ADC_SCALE 4095.0
+    #define VREF 3.3
 #endif
 
 namespace esphome {
@@ -23,15 +23,18 @@ namespace zmpt101b_ns {
 class ZMPT101B
 {
 public:
-	ZMPT101B (uint8_t pin, uint16_t frequency = DEFAULT_FREQUENCY);
-	void     setSensitivity(float value);
-	float 	 getRmsVoltage(uint8_t loopCount = 1);
+    ZMPT101B(uint8_t pin, uint16_t frequency = DEFAULT_FREQUENCY);
+    void setSensitivity(float value);
+    float getRmsVoltage(uint8_t loopCount = 1);
+
+    // Новый метод для мгновенного напряжения
+    float getInstantVoltage();
 
 private:
-	uint8_t  pin;
-	uint32_t period;
-	float 	 sensitivity = DEFAULT_SENSITIVITY;
-	int 	 getZeroPoint();
+    uint8_t pin;
+    uint32_t period;
+    float sensitivity = DEFAULT_SENSITIVITY;
+    int getZeroPoint();
 };
 
 }
