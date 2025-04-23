110662dba002ce98ca6debabedf551bb7f1d413e
Andrey <b_andy@tut.by>
2025-04-23 13:49:20 +0300

    Update zmpt101b_sensor.h

--- components/zmpt101b/sensor/zmpt101b_sensor.h
+++ components/zmpt101b/sensor/zmpt101b_sensor.h
@@ -11,7 +11,11 @@ class ZMPT101BSensor : public PollingComponent, public sensor::Sensor {
  public:
   uint8_t adcPin;
   esphome::zmpt101b_ns::ZMPT101B *zmpt101b;
-  // constructor
+
+  // Новый датчик для мгновенных значений напряжения
+  Sensor *instant_voltage_sensor = new Sensor();
+
+  // Конструктор
   ZMPT101BSensor(uint8_t adcPin, float sensitivity) : PollingComponent(1000) {
     this->adcPin = adcPin;
     this->zmpt101b = new esphome::zmpt101b_ns::ZMPT101B(adcPin);
@@ -21,12 +25,17 @@ class ZMPT101BSensor : public PollingComponent, public sensor::Sensor {
   float get_setup_priority() const override { return esphome::setup_priority::HARDWARE; }
 
   void setup() override {
-    
+    // Ничего не делаем
   }
 
   void update() override {
+    // Публикуем RMS-напряжение
     float Vrms = this->zmpt101b->getRmsVoltage();
     publish_state(Vrms);
+
+    // Публикуем мгновенное напряжение
+    float Vinstant = this->zmpt101b->getInstantVoltage();
+    this->instant_voltage_sensor->publish_state(Vinstant);
   }
 };
 
